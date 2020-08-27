144a7f4cf39d058b95cbba631c70cbdc5f544fe5
Emanuel <emanuelsoda@gmail.com>
2020-08-27 10:11:59 +0200

    comment added to Script

--- Script.R
+++ Script.R
@@ -1,5 +1,6 @@
-# script to create a txt file from the files get from GECO
+# script to create a .txt file from the files get from GECO
 
+# create the dataframe from the input files 
 prepare.data <- function (path){
   fileNames <- Sys.glob(path)
   dataFrame <- data.frame()
@@ -11,12 +12,16 @@ prepare.data <- function (path){
   return(dataFrame)
 }
 
+# create a dataframe for the healthy samples
 data.healthy <- prepare.data("provaData/healthy/*.bed")
 data.healthy["is_healthy"] <- rep(1, times = dim(data.healthy)[1])
 
+# create a dataframe for the tumoral samples
 data.tumor <- prepare.data("provaData/tumor/*.bed")
 data.tumor["is_healthy"] <- rep(0, times = dim(data.tumor)[1])
 
+# combine the two dataframe 
 data <- rbind(data.healthy, data.tumor)
 
+# create a file contaning the data 
 write.table(data, 'data.txt', sep="\t")
